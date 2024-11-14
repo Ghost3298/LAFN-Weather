c01f0f078263f3939730f99cb195b8122c147648
Ghost3298 <eddy.lebbos@hotmail.com>
2024-11-14 22:50:40 +0200

    fixing zoom

--- src/components/Documentations.js
+++ src/components/Documentations.js
@@ -48,7 +48,7 @@ const Documentations = () => {
 
         <MapContainer
           center={[33.8938, 35.5018]} // Default center (Beirut)
-          zoom={8}
+          zoom={7}
           style={{ width: "100%", height: "500px", borderRadius: "8px", boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)" }}
         >
           <TileLayer
