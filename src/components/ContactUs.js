b6a6a645c5c586032a58cf8ac8eadb18cb15bcc4
Ghost3298 <eddy.lebbos@hotmail.com>
2024-11-19 16:52:32 +0200

    fixing forecast in arabic adding location link

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -10,7 +10,7 @@ const ContactUS = () => {
                 <tr>
                     <td>{t('Address')}:</td>
                     <td>
-                        {t('AddressLoc')}
+                        <a href="https://www.google.com/maps/place/Lebanese+Navy+Headquarters/@33.9003517,35.5015699,1446m/data=!3m1!1e3!4m10!1m2!2m1!1sbeirut+naval+base!3m6!1s0x151f16ef69fe029b:0x15f7eb6809c4366b!8m2!3d33.9003521!4d35.5069448!15sChFiZWlydXQgbmF2YWwgYmFzZZIBDW1pbGl0YXJ5X2Jhc2XgAQA!16s%2Fg%2F1tmmhqg4?entry=ttu&g_ep=EgoyMDI0MTExMy4xIKXMDSoASAFQAw%3D%3D" target="_blank" rel="noreferrer">{t('AddressLoc')}</a>
                     </td>
                 </tr>
                 <tr>
